e2179936198615cd06f862ac51ddd5ebdc32c898
GitHub707651707 <707651707@qq.com>
2022-05-20 05:46:48 +0800

    btn admin

---
+++ src/layout/demo/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="header">
+      <slot name="header" /></div>
+    <div class="body">
+      <slot name="body" /></div>
+    <div class="footer">
+      <slot name="footer" /></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-content: center;
+  justify-content: center;
+  .header {
+    width: 100%;
+    height: 200px;
+    background-color: red;
+  }
+  .body {
+    width: 100%;
+    height: 50px;
+    background-color: blue;
+  }
+  .footer {
+    width: 100%;
+    height: 50px;
+    background-color: yellow;
+  }
+}
+</style>
---
+++ src/views/demo/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <DemoLayout>
+      <template slot="header"><Directive /></template>
+      <template slot="body">222</template>
+      <template slot="footer">333</template>
+    </DemoLayout>
+  </div>
+</template>
+
+<script>
+import DemoLayout from '@/layout/demo/index.vue'
+import Directive from './directive.vue'
+
+export default {
+  components: {
+    DemoLayout,
+    Directive
+  }
+}
+
+</script>
